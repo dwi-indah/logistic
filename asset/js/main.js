36dd737659641990bdbd351cdb56f15529d728e1
Dwi Indah <dwindach@gmail.com>
2025-08-27 15:32:43 +0700

    update all

---
+++ asset/js/main.js
@@ -0,0 +1,33 @@
+window.addEventListener('scroll', function() {
+    let navbar = document.querySelector('#navbar');
+    navbar.classList.toggle('fixed-nav', window.scrollY > 0);
+});
+
+const formResi = document.getElementById('form-resi');
+const tracDiv = document.getElementById('trac');
+
+formResi.addEventListener('submit', function(e) {
+    e.preventDefault();
+    tracDiv.classList.add('show');
+    tracDiv.scrollIntoView({
+        behavior: 'smooth',
+        block: 'nearest'
+    });
+    setTimeout(() => {
+        document.querySelector('.trac-animation').classList.remove('show');
+        document.querySelector('.trac-result').classList.add('show');
+    }, 2000);
+})
+
+const navList = document.querySelectorAll('.nav-link');
+const offcanvas = document.querySelector('.offcanvas');
+
+navList.forEach(el => {
+    el.addEventListener('click', function() {
+        offcanvas.classList.remove('show');
+        const offcanvasBg = document.querySelector('.offcanvas-backdrop');
+        offcanvasBg.classList.remove('show');
+        offcanvasBg.remove();
+        document.body.removeAttribute('style');
+    })
+})
